a9e32a95cdab31ae22f561ba99b26686279b270a
luisatapiero <79235499+luisatapiero@users.noreply.github.com>
2021-05-16 13:43:40 -0500

    Created some classes and packages

---
+++ src/model/FoodApp.java
@@ -0,0 +1,9 @@
+package model;
+
+public class FoodApp {
+
+	public FoodApp() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/view/Main.java
@@ -0,0 +1,12 @@
+package view;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+
+	}
+
+}
