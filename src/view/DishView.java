92997ab931a6cfc484d088a6b84017cb71d776bf
yuilsi <yuilsimadeleine@gmail.com>
2021-05-28 20:25:20 -0500

    Interacciones de todos los botones de la pantalla de productos

--- src/view/DishView.java
+++ src/view/DishView.java
@@ -6,7 +6,7 @@ import processing.core.PImage;
 
 public class DishView {
 	private ControllerMain controllermain;
-	private PImage dish_1,dish_2;
+	private PImage dish_1,dish_2,dish_3,dish_4,dish_5;
 	private PApplet app;
 
 	public DishView(PApplet app) {
@@ -16,6 +16,9 @@ public class DishView {
 
 		dish_1 = app.loadImage("img/Dish_1.png");
 		dish_2 = app.loadImage("img/Dish_2.png");
+		dish_3 = app.loadImage("img/Dish_3.png");
+		dish_4 = app.loadImage("img/Dish_4.png");
+		dish_5 = app.loadImage("img/Dish_5.png");
 	}
 
 	// método para pintar la pantalla 
@@ -24,10 +27,20 @@ public class DishView {
 		if (app.mouseX > 34 && 346 > app.mouseX && app.mouseY > 213 && 328 > app.mouseY) {
 			app.image(dish_2, 0, 0);
 
-		} else {
+		} else if(app.mouseX > 34 && 346 > app.mouseX && app.mouseY > 335 && 457 > app.mouseY) {
+			app.image(dish_3, 0, 0);
+			
+		} else if(app.mouseX > 34 && 346 > app.mouseX && app.mouseY > 471 && 588 > app.mouseY) {
+			app.image(dish_4, 0, 0);
+			
+		}  else if(app.mouseX > 34 && 346 > app.mouseX && app.mouseY > 603 && 717 > app.mouseY) {
+			app.image(dish_5, 0, 0);
+		}
+		else {
 			app.image(dish_1, 0, 0);
 		}
-
+		
+		
 	}
 
 	// método para pasar de pantalla
